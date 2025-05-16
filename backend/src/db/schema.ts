e3bbbf4210babdd4f1a0a8f343f5cf7cb74ae45c
Simpl1fy <gourabdas07012004@gmail.com>
2025-05-16 19:09:15 +0530

    added data synchronization to postgreSQL

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,8 @@
 import { timestamp, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: uuid("id").primaryKey(),
+  id: varchar("id", { length: 255 }).primaryKey(),
   email: varchar("email", { length: 255 }).notNull(),
   name: varchar("name", { length: 255 }),
-  createdAt: timestamp("created_at").defaultNow(),
+  createdAt: timestamp("created_at"),
 });
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,26 +2,36 @@ import { Hono } from "hono";
 import { db } from "../db/db";
 import { users } from "../db/schema";
 import { createClerkClient } from "@clerk/backend";
+import { eq } from "drizzle-orm";
 
 const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
 const userRouter = new Hono();
 
-userRouter.get('/sync', async(c) => {
+userRouter.post('/sync', async(c) => {
     try {
         const userId = c.req.header('x-clerk-user-id');
         if(!userId) {
-            return c.json({ error: ""})
+            return c.json({ error: "No user found!" });
         }
-        console.log(userId);
 
         const user = await clerkClient.users.getUser(userId);
-        console.log(user);
+        if(!user) {
+            return c.json({ error: "User not found!" });
+        }
 
-        return c.json({ user }, 200);
+        const userData = await db.select().from(users).where(eq(users.id, userId));
+        console.log(userData);
+        if(userData.length === 0) {
+            await db.insert(users).values({ id: userId, email: user.emailAddresses[0].emailAddress, name: user.firstName+" "+user.lastName, createdAt: new Date(user.createdAt) })
+
+            return c.json({ success: true, message: "Data added to db" }, 200);
+        }
+
+        return c.json({ success: true, message: "Data already present" });
     } catch(err) {
         console.error("An error occured =", err);
         return c.json({ error: "Internal Server Error" }, 500);
     }
-});
+})
 
 export default userRouter;
