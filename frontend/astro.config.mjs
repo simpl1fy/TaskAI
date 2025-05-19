31e7d8c75254c8a09c7160b2891d74b8843d02ed
Simpl1fy <gourabdas07012004@gmail.com>
2025-05-19 18:45:06 +0530

    added netlify ssr adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
+
+// This is for ssr
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 import clerk from "@clerk/astro";
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,8 +18,32 @@ export default defineConfig({
 
   integrations: [react(), clerk()],
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   output: "server",
 });
+
+// SSG
+
+// import * as dotenv from "dotenv";
+// dotenv.config();
+// import { defineConfig } from "astro/config";
+
+// import react from "@astrojs/react";
+
+// import clerk from "@clerk/astro";
+
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+
+//   root: "./src",
+//   integrations: [react(), clerk()],
+
+//   // No adapter means static site build output
+
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+
+//   outDir: "../dist"
+// });
