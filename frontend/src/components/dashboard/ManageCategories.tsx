1ad2e033c3b13d1151c63096188eb60fba467bdd
Gourab Das <gourabdas07012004@gmail.com>
2025-06-30 10:22:02 +0530

    Creating list dialog

---
+++ frontend/src/components/dashboard/ManageCategories.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogTitle,
+    DialogHeader,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+
+type PropTypes = {
+    open: boolean;
+    setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const ManageCategories = ({ open, setOpen }: PropTypes) => {
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+            <Button>Manage Categories</Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogHeader>Manage Categories</DialogHeader>
+                <DialogDescription>Create, Delete, Edit Categories</DialogDescription>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
+  )
+}
+
+export default ManageCategories
