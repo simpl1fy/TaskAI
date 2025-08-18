7738cbf41597048606a76975ef9fe9344481edf4
Gourab Das <gourabdas07012004@gmail.com>
2025-08-18 14:01:23 +0530

    Added gauge graph for progress of tasks

--- frontend/src/components/dashboard/main_screens/analytics_page/analytics.tsx
+++ frontend/src/components/dashboard/main_screens/analytics_page/analytics.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@clerk/clerk-react";
 import { useState, useEffect } from "react";
 import { toast } from "sonner";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
 
 interface TaskData {
   total: number;
@@ -9,8 +10,7 @@ interface TaskData {
   work_in_progress: number;
 }
 
-export default function Analytics () {
-
+export default function Analytics() {
   const { getToken, isSignedIn } = useAuth();
 
   const [data, setData] = useState<TaskData>();
@@ -24,9 +24,9 @@ export default function Analytics () {
         const response = await fetch(`${baseUrl}/analytics/data`, {
           method: "GET",
           headers: {
-            "Authorization": `Bearer ${token}`
-          }
-        })
+            Authorization: `Bearer ${token}`,
+          },
+        });
         const resData = await response.json();
 
         console.log("Received Data = " + resData);
@@ -34,14 +34,30 @@ export default function Analytics () {
         setData(resData);
       } catch (err) {
         console.error("An error occured while fetching tasks number = " + err);
-        toast.error("An error occured while fetching data. Please try again later!");
+        toast.error(
+          "An error occured while fetching data. Please try again later!"
+        );
       }
-    }
+    };
 
-    if(isSignedIn) {
+    if (isSignedIn) {
       fetchAnalyticsData();
     }
   }, [reload]);
+
+  if (!data) {
+    return (
+      <div className="p-5">
+        <header className="mb-5">
+          <h3 className="text-2xl">Analytics</h3>
+          <p className="text-gray-700 text-md">
+            View stats, check your progress of work completed.
+          </p>
+        </header>
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="px-5 py-5">
@@ -51,20 +67,77 @@ export default function Analytics () {
           View stats, check your progress of work completed.
         </p>
       </header>
-      <section className="grid grid-cols-3 gap-4">
-        <div className="rounded-lg shadow-sm bg-green-100 p-3 shadow-green-200">
-          <h3 className="text-lg font-bold">Tasks Completed: </h3>
-          <span className="text-xl text-green-800">{data?.completed}/{data?.total}</span>
+      <section className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col items-center">
+          <h3>Completed Tasks</h3>
+          <Gauge
+            value={data?.completed}
+            valueMax={data?.total}
+            height={250}
+            cornerRadius="50%"
+            startAngle={-150}
+            endAngle={150}
+            text={({ value, valueMax }) => `${value}/${valueMax}`}
+            sx={(theme) => ({
+              [`& .${gaugeClasses.valueText}`]: {
+                fontSize: 30,
+              },
+              [`& .${gaugeClasses.valueArc}`]: {
+                fill: "#00c950",
+              },
+              [`& .${gaugeClasses.referenceArc}`]: {
+                fill: '#f0fdf4',
+              },
+            })}
+          />
         </div>
-        <div className="rounded-lg shadow-sm bg-yellow-100 p-3 shadow-yellow-200">
-          <h3 className="text-lg font-bold">Tasks In Progress: </h3>
-          <span className="text-xl text-yellow-800">{data?.work_in_progress}/{data?.total}</span>
+        <div className="flex flex-col items-center">
+          <h3>In Progress</h3>
+          <Gauge
+            value={data.work_in_progress}
+            valueMax={data?.total - data?.completed}
+            height={250}
+            cornerRadius="50%"
+            startAngle={-150}
+            endAngle={150}
+            text={({ value, valueMax }) => `${value}/${valueMax}`}
+            sx={(theme) => ({
+              [`& .${gaugeClasses.valueText}`]: {
+                fontSize: 30,
+              },
+              [`& .${gaugeClasses.valueArc}`]: {
+                fill: "#f0b100",
+              },
+              [`& .${gaugeClasses.referenceArc}`]: {
+                fill: '#fef9c2',
+              },
+            })}
+          />
         </div>
-        <div className="rounded-lg shadow-sm bg-red-100 p-3 shadow-red-200">
-          <h3 className="text-lg font-bold">Tasks Incomplete: </h3>
-          <span className="text-xl text-red-800">{data?.incomplete}/{data?.total}</span>
+        <div className="col-span-2 flex flex-col items-center">
+          <h3>Incomplete</h3>
+          <Gauge
+            value={data.incomplete}
+            valueMax={data?.total-data.completed}
+            height={250}
+            cornerRadius="50%"
+            startAngle={-150}
+            endAngle={150}
+            text={({ value, valueMax }) => `${value}/${valueMax}`}
+            sx={(theme) => ({
+              [`& .${gaugeClasses.valueText}`]: {
+                fontSize: 30,
+              },
+              [`& .${gaugeClasses.valueArc}`]: {
+                fill: "#fb2c36",
+              },
+              [`& .${gaugeClasses.referenceArc}`]: {
+                fill: '#ffe2e2',
+              },
+            })}
+          />
         </div>
       </section>
     </div>
-  )
+  );
 }
