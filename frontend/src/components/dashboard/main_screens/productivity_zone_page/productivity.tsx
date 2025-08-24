99630f6f474f4452682c7dee346864c77cecfc31
Gourab Das <gourabdas07012004@gmail.com>
2025-08-25 01:33:50 +0530

    Added tool tip for pomodoro text

--- frontend/src/components/dashboard/main_screens/productivity_zone_page/productivity.tsx
+++ frontend/src/components/dashboard/main_screens/productivity_zone_page/productivity.tsx
@@ -1,15 +1,23 @@
-import { useMemo } from "react";
+import { useState, useMemo } from "react";
 import Timer from "./components/timer";
 import TimerButtons from "./components/buttons";
 import { useTimer } from "@/context/useTimer";
 
 export default function Productivity() {
-  
   const {
-    status, timerType, workTime, breakTime,
-    start, pause, stop,
-    sessionIterations, sessionTotalWorkTime, formatHMS
+    status,
+    timerType,
+    workTime,
+    breakTime,
+    start,
+    pause,
+    stop,
+    sessionIterations,
+    sessionTotalWorkTime,
+    formatHMS,
   } = useTimer();
+
+  const [pomodoroHover, setPomodoroHover] = useState(false);
 
   const active = status !== "idle";
   const paused = status === "paused";
@@ -20,20 +28,51 @@ export default function Productivity() {
     else stop();
   };
 
-  const timeDisplay = useMemo(() => formatHMS(sessionTotalWorkTime), [sessionTotalWorkTime]);
+  const timeDisplay = useMemo(
+    () => formatHMS(sessionTotalWorkTime),
+    [sessionTotalWorkTime]
+  );
 
   return (
     <div className="px-5 py-5">
       <header>
         <h3 className="text-2xl">Productivity Zone</h3>
         <p className="text-gray-700 text-md">
-          Start studying and working with POMODORO technique and boost your productivity
+          Start studying and working with{" "}
+          <span
+            className="underline bg-amber-100 text-black decoration-blue-500 relative"
+            onPointerEnter={() => setPomodoroHover(true)}
+            onPointerLeave={() => setPomodoroHover(false)}
+          >
+            <span className="cursor-pointer">POMODORO</span>
+            {pomodoroHover && (
+              <div className="absolute left-0 top-4 shadow-sm p-2 rounded-md min-w-sm">
+                <p className="text-xs">
+                  The Pomodoro Technique is a time-management method that uses a
+                  timer to break work into 25-minute intervals, called
+                  "pomodoros," separated by short 5-minute breaks.{" "}
+                  <a
+                    href="https://en.wikipedia.org/wiki/Pomodoro_Technique"
+                    className="text-blue-500 cursor-pointer"
+                    target="_blank"
+                  >
+                    Read More
+                  </a>
+                </p>
+              </div>
+            )}
+          </span>{" "}
+          technique and boost your productivity
         </p>
       </header>
 
-      <div className="grid grid-cols-4">
-        <section className="flex flex-col items-center gap-10 mt-10 col-span-3">
-          <Timer workTime={workTime} breakTime={breakTime} timerType={timerType} />
+      <div className="flex justify-center items-center">
+        <section className="flex flex-col items-center gap-10 mt-10 mb-10">
+          <Timer
+            workTime={workTime}
+            breakTime={breakTime}
+            timerType={timerType}
+          />
           <TimerButtons
             handleStart={handleStart}
             active={active}
@@ -41,14 +80,18 @@ export default function Productivity() {
             handleStopPause={handleStopPause}
           />
         </section>
+      </div>
 
-        <div className="shadow-sm rounded-lg p-4 col-span-1">
-          <div className="text-gray-700">
-            Iterations this session: <span className="font-bold text-black">{Math.floor(sessionIterations)}</span>
-          </div>
-          <div className="text-gray-700">
-            Time spent in session: <span className="font-bold text-black">{timeDisplay}</span>
-          </div>
+      <div className="shadow-sm rounded-lg p-4 max-h-fit max-w-fit">
+        <div className="text-gray-700">
+          Iterations this session:{" "}
+          <span className="font-bold text-black">
+            {Math.floor(sessionIterations)}
+          </span>
+        </div>
+        <div className="text-gray-700">
+          Time spent in session:{" "}
+          <span className="font-bold text-black">{timeDisplay}</span>
         </div>
       </div>
     </div>
